d035020b399ea967ff42a233557b9ad27ee4c42a
Harrie Verveer <harrieverveer@gmail.com>
2013-05-10 15:44:02 +0200

    Added multiple parameters to the with clause

--- unittests/src/Hairy/Model/UserTest.php
+++ unittests/src/Hairy/Model/UserTest.php
@@ -6,6 +6,12 @@ class UserTest extends PHPUnit_Framework_TestCase
         $mockMailer = $this->getMock('Hairy\Lib\Mailer', array('send'));
         $mockMailer->expects($this->once())
                    ->method('send')
+                   ->with(
+                       $this->equalTo('johndoe@mydomain'),
+                       $this->equalTo('John Doe'),
+                       $this->isType('string'),
+                       $this->stringContains('This is my message')
+                   )
                    ->will($this->returnValue(true));
         
         $userModel = new Hairy\Model\User('John', 'Doe', 'johndoe@mydomain');
